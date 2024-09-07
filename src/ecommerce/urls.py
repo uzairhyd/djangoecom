3283b3ba89f8149d368dd1de10f44053516be578
uzairhyd <uzairhyd89@gmail.com>
2024-09-07 19:10:41 +0530

    updated the code for detail view

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -19,8 +19,8 @@ from django.conf.urls.static import static
 
 from django.contrib import admin
 from django.urls import path
-
-
+  
+from products.views import ProductListView, ProductDetailView
 
 from .views import home_page, about_page, contact_page, login_page, register_page
 
@@ -30,6 +30,8 @@ urlpatterns = [
     path('contact/',contact_page),
     path('login/', login_page),
     path('register/', register_page),
+    path('products/', ProductListView.as_view()),
+    path('products/<int:pk>/', ProductDetailView.as_view()),
     path('admin/', admin.site.urls),
 ]
 
--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,19 @@
+
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 
+from .models import Product
 # Create your views here.
+
+class ProductListView(ListView):
+    queryset = Product.objects.all()
+    template_name = "products/list.html"
+
+    # def get_context_data(self,*args, **kwargs):
+    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
+    #     print(context)
+    #     return context
+
+class ProductDetailView(DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
